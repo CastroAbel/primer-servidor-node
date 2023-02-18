3a090f2585ff0e498af4b03e762852d40efd15cd
Abel Castro <abelcastrohoyos@gmail.com>
2023-02-18 15:43:24 -0500

    Nuestro primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',function(req,res){
+    res.send("Hola mundo!!")
+})
+
+app.listen(3000)
+
+function suma(a,b) {
+  return a + b
+}
